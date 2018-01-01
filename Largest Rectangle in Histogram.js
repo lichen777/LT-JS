e5cb944439bd1ccb43d3805707d356280d17118a
lichen777 <lchen139@gmail.com>
2018-01-01 13:07:29 -0600

    Update Largest Rectangle in Histogram.js

--- Largest Rectangle in Histogram.js
+++ Largest Rectangle in Histogram.js
@@ -7,12 +7,12 @@ var largestRectangleArea = function(heights) {
     if (len === 0) {
         return 0;
     }
+    heights.push(-1);
     
     var stack = [];
     var max = 0;
-    for (var i = 0; i <= heights.length; i++) {
-        var cur = (i === len) ? -1 : heights[i];
-        while (stack.length > 0 && cur <= heights[stack[stack.length - 1]]) {
+    for (var i = 0; i <= len; i++) {
+        while (stack.length > 0 && heights[i] <= heights[stack[stack.length - 1]]) {
             var h = heights[stack.pop()];
             var w = stack.length > 0 ? i - stack[stack.length - 1] - 1 : i;
             max = Math.max(max, h * w);
