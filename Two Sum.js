d29ee53aa850c3d75498de86a1cd1a9000b29de6
Chen Li <lchen139@gmail.com>
2018-01-11 14:32:08 -0600

    Update Two Sum.js

--- Two Sum.js
+++ Two Sum.js
@@ -1,18 +1,19 @@
 /**
+https://leetcode.com/problems/two-sum/description/
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let result = [];
-    let map = {};
-    for (var i = 0; i < nums.length; i++){
-        let valueNeeded = target - nums[i];
-        if (map[nums[i]] !== undefined){
-            result.push(map[nums[i]], i);
-        } else {
-            map[valueNeeded] = i;
+    var result = [];
+    var map = {};
+    for (var i = 0; i < nums.length; i++) {
+        var valueNeeded = target - nums[i];
+        if (map[valueNeeded] !== undefined) {
+            result.push(map[valueNeeded], i);
+            return result;
         }
+        map[nums[i]] = i;
     }
     return result;
 };
