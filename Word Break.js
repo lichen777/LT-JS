0955f4275fa98d240dc4bf89911e0a4ae5f028b0
Chen Li <lchen139@gmail.com>
2018-01-16 15:21:25 -0600

    Create Word Break.js

---
+++ Word Break.js
@@ -0,0 +1,29 @@
+https://leetcode.com/problems/word-break/description/
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+    if (wordDict === null || wordDict.length === 0) {
+        return false;
+    }
+    var found = [];
+    found[0] = true;
+    var maxLen = 0;
+    for (var word of wordDict) {
+        maxLen = Math.max(maxLen, word.length);
+    }
+    for (var i = 1; i <= s.length; i++) {
+        for (var j = 1; j <= Math.min(maxLen, i); j++){
+            if (found[i-j]) {
+              var str = s.substring(i-j, i);
+                if (wordDict.indexOf(str) !== -1) {
+                    found[i] = true;
+                    break;
+                }  
+            }   
+        }        
+    }
+    return found[s.length] === true;
+};
