6270529e3c32d437d10843494be911e3d02b7561
Chen Li <lchen139@gmail.com>
2018-01-21 22:24:09 -0600

    Create Edit Distance.js

---
+++ Edit Distance.js
@@ -0,0 +1,34 @@
+https://leetcode.com/problems/edit-distance/description/
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+    var m = word1.length;
+    var n = word2.length;
+    var f = [];
+    
+    if (m === 0 || n === 0) {
+      return Math.max(m, n);
+    }
+    
+    for (var i = 0; i <= m; i++) {
+      f[i] = [];
+      f[i][0] = i;
+    }
+    for (var j = 0; j <= n; j++) {
+      f[0][j] = j;
+    }
+    
+    for (var i = 1; i <= m; i++) {
+      for (var j = 1; j <= n; j++) {
+        if (word1.charAt(i - 1) === word2.charAt(j - 1)) {
+          f[i][j] = f[i - 1][j - 1];
+        } else {
+          f[i][j] = Math.min(f[i - 1][j], f[i][j - 1], f[i - 1][j - 1]) + 1;
+        }
+      }
+    }
+    return f[m][n];
+};
