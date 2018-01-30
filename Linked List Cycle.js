cdb2802712824ba86b0594b4217ef77c96cba5a5
Chen Li <lchen139@gmail.com>
2018-01-30 11:14:40 -0600

    Create Linked List Cycle.js

---
+++ Linked List Cycle.js
@@ -0,0 +1,28 @@
+https://leetcode.com/problems/linked-list-cycle/description/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(head === null || head.next === null) {
+        return false;
+    }
+    var fast = head.next;
+    var slow = head;
+    while (fast !== slow) {
+        if(fast == null || fast.next === null) {
+            return false
+        }
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return true;
+};
